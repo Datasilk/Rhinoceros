10a7a73df2b66663922722765efec34334b31328
Mark Entingh <entingh@gmail.com>
2018-04-27 16:43:41 -0700

    added CefSharp and created working prototype

---
+++ App/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CefSharp.Wpf;
+
+namespace Rhinoceros
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            browser.JavascriptObjectRepository.Register("Rhino", new JsCallback(this), true);
+        }
+
+    }
+
+    public class JsCallback
+    {
+        MainWindow main;
+        public JsCallback(MainWindow main) { this.main = main; }
+
+        #region "Callback Methods used via Javascript"
+        public void Exit()
+        {
+            Application.Current.Shutdown();
+        }
+
+        public void Maximize()
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+        }
+
+        public void Minimize()
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        public void Toolbar(bool show)
+        {
+            if(show == true)
+            {
+                main.WindowStyle = WindowStyle.ToolWindow;
+            }
+            else
+            {
+                main.WindowStyle = WindowStyle.SingleBorderWindow;
+            }
+        }
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -11,3 +11,26 @@ Create desktop applications with ease by combining any website with a borderless
 * Includes an installer used to redistribute your custom application
 * Allow user to create desktop shortcuts to their favorite websites, transforming them into desktop applications
 * Option to display a built-in toolbar at the top of the window (if the website doesn't have it's own custom designed toolbar)
+
+### Javascript Binding
+You can execute certain methods within Rhinoceros via JavaScript in order to manipulate the browser window. To allow JavaScript binding, add the following code to your web page:
+
+```
+<script type="text/javascript">
+    (async function() {
+        await CefSharp.BindObjectAsync("Rhino", "bound");
+
+		//add your code below
+		Rhino.Maximize();
+    })();
+</script>
+```
+
+The following methods are supported via JavaScript:
+
+Method|Arguments|Definition
+---|---
+Rhino.Maximize||Maximize the browser window
+Rhino.Minimize||Minimize the browser window
+Rhino.Exit||Close the browser window (and application)
+Rhino.Toolbar|bool show|Show or hide built-in window toolbar
